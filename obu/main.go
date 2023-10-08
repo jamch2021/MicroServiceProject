aec30618a46ed83c4d5fecd7f8cb472b71de7ea4
jamch2021 <jmc237@njit.edu>
2023-10-08 14:09:37 -0400

    Add init files

---
+++ obu/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/jamch2021/microserviceProject/types"
+)
+const wsEndPoint = "ws://127.0.0.1:30000/ws"
+
+var sendInterval = time.Second
+
+
+func genCoord() float64 {
+	n := float64(rand.Intn(100) + 1)
+	f := rand.Float64()
+
+	return n+f
+}
+
+func genLatLong() (float64, float64) {
+	return genCoord(), genCoord()
+}
+
+
+func main() {
+	obuIDS := generateOBUIDS(20)
+	conn, _, err := websocket.DefaultDialer.Dial(wsEndPoint, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	for {
+		for i:=0; i<len(obuIDS); i++ {
+			lat, long := genLatLong()
+			data := types.OBUData {
+				OBUID: obuIDS[i],
+				Lat : lat,
+				Long: long,
+			}
+
+			if err := conn.WriteJSON(data); err != nil {
+				log.Fatal(err)
+			}
+			//fmt.Printf("%+v\n", data)
+		}
+		time.Sleep(sendInterval)
+	}
+}
+
+func generateOBUIDS(n int) []int {
+	ids := make([]int, n)
+	for i :=0; i < n; i++ {
+		ids[i] = rand.Intn(math.MaxInt)
+	}
+	return ids
+}
+
+// func Init() {
+// 	rand.Seed(time.Now().UnixNano())
+// }
